ec22bab0e741ff68d00165a5a76ab60885f67bb1
Adam J.A <22190@ecam.be>
2025-02-11 11:04:37 +0100

    modif

--- Kitbox/ViewModels/Seller.cs
+++ Kitbox/ViewModels/Seller.cs
@@ -6,10 +6,20 @@ namespace Pong.ViewModels;
 
 public partial class Seller{
     public string email;
-    public Seller(string email){
-        this.email = email;    
+    public enum Status {totalypaid, partiallypaid, notpaid};
+    public Status status;
+    public 
+
+    public Seller(string email, Status status){
+        this.email = email;
+        this.status = status;
+
+        
+         
+
+          
     }
-    
+
 
 
 
