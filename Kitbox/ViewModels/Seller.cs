2856a4ca8eb3d010cf805a9e6e6204cfd5b387e2
Adam J.A <22190@ecam.be>
2025-02-11 10:46:19 +0100

    sellerupdate

--- Kitbox/ViewModels/Seller.cs
+++ Kitbox/ViewModels/Seller.cs
@@ -1,3 +1,16 @@
-class Seller{
+using System;
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Pong.ViewModels;
+
+public partial class Seller{
+    public string email;
+    public Seller(string email){
+        this.email = email;    
+    }
     
+
+
+
 }
