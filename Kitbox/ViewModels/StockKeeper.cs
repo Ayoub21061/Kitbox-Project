b427a0e7071b551393aa4b24115f2c244637d091
Yildirim Arifcan <Arifcan091@gmail.com>
2025-02-11 11:15:05 +0100

    Adding class User

---
+++ Kitbox/ViewModels/StockKeeper.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls.Shapes;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Kitbox.ViewModels;
+using System;
+
+namespace Kitbox.ViewModels;
+
+// représente un objet affichable sur l'interface
+public abstract class StockKeeper : User 
+{
+    
+
+   
+   
+    // New method to update life and energy
+   
+
+}
