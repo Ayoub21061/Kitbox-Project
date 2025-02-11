cc2a03fb23926e2ab9f294cea446f14384e739d7
Adam J.A <22190@ecam.be>
2025-02-11 11:03:04 +0100

    modiflegere

--- Kitbox/obj/Debug/net8.0/Kitbox.AssemblyInfo.cs
+++ Kitbox/obj/Debug/net8.0/Kitbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kitbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a9b231b3a5f79a8084ae6b11af59cf362555ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kitbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kitbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
