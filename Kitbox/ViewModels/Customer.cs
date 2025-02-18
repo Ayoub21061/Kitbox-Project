6b8da857563b2bf88a268f6a745826e3acfae27d
Ayoub21061 <21061@ecam.be>
2025-02-18 13:42:29 +0100

    Save button and image added

--- Kitbox/ViewModels/Customer.cs
+++ Kitbox/ViewModels/Customer.cs
@@ -13,32 +13,18 @@ namespace Kitbox.Models
         [ObservableProperty]
         private int depth;
 
-        [ObservableProperty]
-        private int doorCount;
-
-        [ObservableProperty]
-        private int shelfCount;
-
-        [ObservableProperty]
-        private string color;
 
         public Customer() // Constructeur par défaut 
         {
             Height = 0;
             Width = 0;
             Depth = 0;
-            DoorCount = 0;
-            ShelfCount = 0;
-            Color = "Blanc";
         }
-        public Customer(int height, int width, int depth, int doorCount, int shelfCount, string color) 
+        public Customer(int height, int width, int depth) 
         {
             Height = height;
             Width = width;
             Depth = depth;
-            DoorCount = doorCount;
-            ShelfCount = shelfCount;
-            Color = color;
         }
 
         
