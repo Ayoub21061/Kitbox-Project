7d0a5ba687d26fbbbfb2e12ae21a011eeb8f7d3d
Adam J.A <22190@ecam.be>
2025-04-27 16:35:48 +0200

    basepagetransfert

--- Kitbox/ViewModels/MainWindowViewModel.cs
+++ Kitbox/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,39 @@
-﻿namespace Kitbox.ViewModels
+﻿using ReactiveUI;
+using System.Reactive;
+using Kitbox.ViewModels;
+
+namespace Kitbox.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ReactiveObject
     {
-        public SellerViewModel SellerViewModel { get; } = new SellerViewModel();
-        
+        private ReactiveObject _currentViewModel;
+        public ReactiveObject CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => this.RaiseAndSetIfChanged(ref _currentViewModel, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> ShowSellerViewCommand { get; }
+        public ReactiveCommand<Unit, Unit> ShowEmptyViewCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            // Par défaut, commencer avec SellerView
+            CurrentViewModel = new SellerViewModel();
+            
+            ShowSellerViewCommand = ReactiveCommand.Create(
+                () => {
+                    CurrentViewModel = new SellerViewModel();
+                    return Unit.Default;
+                }
+            );
+
+            ShowEmptyViewCommand = ReactiveCommand.Create(
+                () => {
+                    CurrentViewModel = new EmptyViewModel();
+                    return Unit.Default;
+                }
+            );
+        }
     }
 }
---
+++ Kitbox/Views/EmptyView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Kitbox.Views
+{
+    public partial class EmptyView : UserControl
+    {
+        public EmptyView()
+        {
+            InitializeComponent();
+        }
+
+        // Gérer le clic du bouton OK
+        
+    }
+}
+
